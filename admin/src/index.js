ce44deefe5ce04c2a9d7aacddcd9a3ac43df3fff
Elisha-Kiplangat <elishakiplanga@gmail.com>
2025-03-15 01:20:56 +0300

    messages and notification

--- admin/src/index.js
+++ admin/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { DarkModeContextProvider } from "./context/darkModeContext";
 import { AuthContextProvider } from "./context/AuthContext";
+import { SocketContextProvider } from "./context/SocketContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthContextProvider>
       <DarkModeContextProvider>
+        <SocketContextProvider>
         <App />
+        </SocketContextProvider>
       </DarkModeContextProvider>
     </AuthContextProvider>
   </React.StrictMode>,
