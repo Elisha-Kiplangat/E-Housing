4e33c7258d89f194978286b545977f3933a7874e
Elisha-Kiplangat <elishakiplanga@gmail.com>
2025-03-15 01:19:33 +0300

    profile section

--- admin/src/components/navbar/Navbar.jsx
+++ admin/src/components/navbar/Navbar.jsx
@@ -1,29 +1,63 @@
 import "./navbar.scss";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+// import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
-import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+// import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
-import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
+// import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 import { DarkModeContext } from "../../context/darkModeContext";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { useNotificationStore } from "../../lib/notificationStore";
+import Chat from "../chat/Chat.jsx";
 
-const Navbar = () => {
+const Navbar = ({ setSearchQueryProp }) => {
   const { dispatch } = useContext(DarkModeContext);
+  const [searchQuery, setSearchQuery] = useState("");
+  const { user } = useContext(AuthContext);
+  const fetch = useNotificationStore((state) => state.fetch);
+  const number = useNotificationStore((state) => state.number);
+  const [isChatOpen, setIsChatOpen] = useState(false);
+  
+    if (user) fetch();
+
+    // console.log(user);
+
+  useEffect(() => {
+    if(setSearchQueryProp) {
+      setSearchQueryProp(searchQuery);
+    }
+  }, [searchQuery, setSearchQueryProp]);
+
+  const handleOpenChat = async () => {
+    setIsChatOpen(true);
+  }
+
+  const handleCloseChat = () => {
+    setIsChatOpen(false);
+  }
 
   return (
+    <>
     <div className="navbar">
       <div className="wrapper">
         <div className="search">
-          <input type="text" placeholder="Search..." />
-          <SearchOutlinedIcon />
+            <input
+              type="text"
+              placeholder="Search..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button type="submit" className="searchButton">
+              <SearchOutlinedIcon />
+            </button>
         </div>
         <div className="items">
-          <div className="item">
+          {/* <div className="item">
             <LanguageOutlinedIcon className="icon" />
             English
-          </div>
+          </div> */}
           <div className="item">
             <DarkModeOutlinedIcon
               className="icon"
@@ -31,22 +65,22 @@ const Navbar = () => {
             />
           </div>
           <div className="item">
-            <FullscreenExitOutlinedIcon className="icon" />
+            {/* <FullscreenExitOutlinedIcon className="icon" /> */}
           </div>
           <div className="item">
             <NotificationsNoneOutlinedIcon className="icon" />
-            <div className="counter">1</div>
+            { number > 0 && <div className="counter">{number}</div> }
           </div>
-          <div className="item">
+          <div className="item" onClick={handleOpenChat}>
             <ChatBubbleOutlineOutlinedIcon className="icon" />
-            <div className="counter">2</div>
+            { number > 0 && <div className="counter">{number}</div> }
           </div>
           <div className="item">
-            <ListOutlinedIcon className="icon" />
+            {/* <ListOutlinedIcon className="icon" /> */}
           </div>
           <div className="item">
             <img
-              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              src={user?.avatar || "/noavatar.jpg"}
               alt=""
               className="avatar"
             />
@@ -54,6 +88,8 @@ const Navbar = () => {
         </div>
       </div>
     </div>
+        {isChatOpen && <Chat handleCloseChat={handleCloseChat} />}
+        </>
   );
 };
 
