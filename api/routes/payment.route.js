8604a39a6889a201837e20713b39ce6761a1e38a
Elisha-Kiplangat <elishakiplanga@gmail.com>
2025-03-27 00:57:26 +0300

    payment updates

--- api/routes/payment.route.js
+++ api/routes/payment.route.js
@@ -1,16 +1,16 @@
 import express from "express";
-import { checkPaymentStatus, createPayment, getMonthlyPaymentStats, getPayments, getPaymentStats, mpesaCallback, payment, totalPayments } from "../controllers/payment.controller.js";
-import { verifyToken } from "../middleware/verifyToken.js";
+import { createPayment, getMonthlyPaymentStats, getPayments, getPaymentStats, payment, totalPayments } from "../controllers/payment.controller.js";
+// import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.post("/mpesa-callback", mpesaCallback);
+// router.post("/mpesa-callback", mpesaCallback);
 
 router.get("/", getPayments);
 router.get("/total", totalPayments);
 router.get("/stats", getPaymentStats);
 router.get("/monthly-stats", getMonthlyPaymentStats);
-router.get("/status/:checkoutRequestId", checkPaymentStatus); //added 
+// router.get("/status/:checkoutRequestId", checkPaymentStatus); 
 router.post("/save", createPayment)
 router.post("/", payment );
 
