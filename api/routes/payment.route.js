b62e83d7c138792498faa676489a051910641eec
Elisha-Kiplangat <elishakiplanga@gmail.com>
2025-03-26 10:29:58 +0300

    Callback route

--- api/routes/payment.route.js
+++ api/routes/payment.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { checkPaymentStatus, createPayment, getMonthlyPaymentStats, getPayments, getPaymentStats, payment, totalPayments } from "../controllers/payment.controller.js";
+import { checkPaymentStatus, createPayment, getMonthlyPaymentStats, getPayments, getPaymentStats, mpesaCallback, payment, totalPayments } from "../controllers/payment.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
+
+router.post("/mpesa-callback", mpesaCallback);
 
 router.get("/", getPayments);
 router.get("/total", totalPayments);
