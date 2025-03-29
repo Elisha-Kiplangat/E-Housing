4ebedab72f83cb09f109f04373f54cf2386b01fa
Elisha-Kiplangat <elishakiplanga@gmail.com>
2025-03-29 23:25:03 +0300

    update

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -67,7 +67,7 @@ export const login = async (req, res) => {
 
     res
       .cookie("token", token, {
-        // httpOnly: true,
+        httpOnly: true,
         secure:true,
         sameSite: "None",
         maxAge: age,
